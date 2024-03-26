98595270bf7d873c5ec9c27d89494f4e6aecfae5
echjansen <echjansen@outlook.com>
2024-03-27 10:21:08 +1100

    init: - Added 'init.el'. Start of Emacs configuration.

---
+++ init.el
@@ -0,0 +1,31 @@
+;;; init.el --- Pure Emacs initialization. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 echjansen
+
+;; This file is part of = P U R E - E M A C S =
+;;
+;; pure-emacs is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; pure-emacs is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with pure-emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Early decisions
+;; - Fast startup time - achieved with byte compiling and use-package.
+;; - Emacs build in features - unless inferior or non existing.
+;; - Package.el package manager - has all features required (including vc)
+
+;;; Code:
+
+
+(provide 'init)
+;;; init.el ends here
