cb39e5e86f3626f94989cf59ab97dd358d7ce775
echjansen <echjansen@outlook.com>
2024-03-27 11:03:19 +1100

    use-package: - Added 'use-package'. Package configuration macro.

--- modules/pure-emacs.el
+++ modules/pure-emacs.el
@@ -27,6 +27,28 @@
 
 ;;; Code:
 
+;;;; Package Configuration
+
+;;;;; = use-package - package configuration macro
+;; Built-in Emacs since version 29 and provides a 'consistent' package
+;; configuration.
+;; To get statistics on package load times start Emacs with --debug-init.
+;; This will compute the statistics which can be called with
+;; M-X use-package-report
+;; Note the eval-when-compile - extracting use-package only during
+;; compilation. The expanded code is used during evaluation.
+(eval-when-compile
+  (require 'use-package)
+  (if init-file-debug
+      (progn
+        (message "--debug-init active")
+        (setq use-package-compute-statistics t)
+        (setq use-package-verbose t)
+        (setq use-package-minimum-reported-time 0.001))
+    (setq use-package-compute-statistics nil))
+  (setq use-package-always-ensure t)
+  (setq use-package-always-defer t)
+  (setq use-package-expand-minimally t))
 
 (provide 'pure-emacs)
 ;;; pure-emacs.el ends here
